ee038ce381063c8f97113e2d158bf7d113f03005
Nigthshitf <burgos3du@gmail.com>
2025-03-28 19:55:46 -0400

    Implementacion de Yup para Formik en el Formulario de Creación de Procesos de Graduación

--- src/pages/CreateGraduation/components/ProcessForm.tsx
+++ src/pages/CreateGraduation/components/ProcessForm.tsx
@@ -17,6 +17,7 @@ import { Modes } from "../../../models/modeInterface";
 import { createGraduationProcess } from "../../../services/processServicer";
 import { useNavigate } from "react-router-dom";
 import { useProcessStore } from "../../../store/store";
+import * as yup from "yup";
 
 function ProcessForm() {
   const [, setError] = useState<string | null>(null);
@@ -25,6 +26,43 @@ function ProcessForm() {
   const updateProcess = useProcessStore((state) => state.setProcess);
   const navigate = useNavigate();
 
+
+
+  const validationSchema = yup.object().shape({
+    studentId: yup
+      .number()
+      .typeError("El ID del estudiante debe ser un número")  
+      .integer("El ID debe ser un número entero")
+      .positive("El ID debe ser un número positivo")
+      .required("Campo requerido"),
+  
+    studentCode: yup
+      .number()
+      .typeError("El código del estudiante debe ser un número")
+      .integer("El código debe ser un número entero")
+      .positive("El código debe ser positivo")
+      .required("Campo requerido"),
+  
+    modeId: yup
+      .number()
+      .typeError("El ID de modalidad debe ser un número")
+      .integer("Debe ser un número entero")
+      .positive("Debe ser un número positivo")
+      .required("Campo requerido"),
+  
+    period: yup
+      .string()
+      .oneOf(["Segundo2024", "Primero2025", "Segundo2025"], "Periodo inválido")  // Asegura valores válidos
+      .required("Campo requerido"),
+  
+    titleProject: yup
+      .string()
+      .min(5, "El título debe tener al menos 5 caracteres")
+      .max(80, "El título no debe superar los 80 caracteres")
+      .matches(/^[a-zA-Z0-9\s]+$/, "El título solo debe contener letras y números")
+      .required("Campo requerido"),
+  });
+  
   const fetchData = useCallback(async () => {
     try {
       const responseStudents = await getStudents();
@@ -41,15 +79,18 @@ function ProcessForm() {
     fetchData();
   }, [fetchData]);
 
+
+
   const formik = useFormik({
     initialValues: {
-      studentId: "",
-      studentCode: "",
-      modeId: "",
-      period: "",
-      titleProject: "",
+      studentId: '',
+      studentCode: '',
+      modeId: '',
+      period: '',
+      titleProject: '',
       stageId: 1,
     },
+    validationSchema: validationSchema,
     onSubmit: async (values) => {
       const response = await createGraduationProcess(values);
       if (response.success) {
