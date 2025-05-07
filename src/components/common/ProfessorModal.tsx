ef7a2621c8aff7f2fcd1423e77f2f191b656a9bd
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2025-05-07 13:27:12 -0400

    feat(forms_to_modals): [316] professor edit and creation now are on modals

---
+++ src/components/common/ProfessorModal.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Modal as MuiModal, Box } from "@mui/material";
+import EditProfessorComponent from "./EditProfessorComponent";
+import CreateProfessorForm from "../../pages/Professor/CreateProfessorForm";
+
+type funcType = "edit" | "create";
+interface ModalProps {
+  open: boolean;
+  onClose: () => void;
+  func: funcType;
+  id: number | null;
+}
+
+const Modal: React.FC<ModalProps> = ({ open, onClose, func, id }) => {
+  return (
+    <MuiModal open={open} onClose={onClose}>
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          backgroundColor: "#ffffff",
+          border: "2px solid #000",
+          maxHeight: "90vh",
+          boxShadow: 24,
+          padding: "16px",
+          borderRadius: "8px",
+          overflowY: "auto",
+        }}
+      >
+        {func == "create" && <CreateProfessorForm onSuccess={onClose} />}
+        {func == "edit" && <EditProfessorComponent id={id ?? -1} onClose={onClose} />}
+      </Box>
+    </MuiModal>
+  );
+};
+
+export default Modal;
