14e4780fb1dd1661735868d308777a0b1f8cd471
ZeinTonconi <zein@DESKTOP-37TT2H8.>
2025-05-08 21:52:35 -0400

    Fix: Autoselect Tutor and Reviewer on Stage 4

--- src/components/stages/InternalDefenseStage.tsx
+++ src/components/stages/InternalDefenseStage.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import dayjs, { Dayjs } from "dayjs";
 import { Box, Grid, Button, Typography } from "@mui/material";
 
@@ -38,7 +38,7 @@ interface InternalDefenseStageProps {
 
 const currentDate = dayjs();
 
-export const InternalDefenseStage: FC<InternalDefenseStageProps> = ({ onPrevious, onNext }) => {
+const InternalDefenseStage: FC<InternalDefenseStageProps> = ({ onPrevious, onNext }) => {
   const [showModal, setShowModal] = useState<boolean>(false);
   const [readOnly, setReadOnly] = useState<boolean>(true);
 
@@ -50,8 +50,9 @@ export const InternalDefenseStage: FC<InternalDefenseStageProps> = ({ onPrevious
   const formik = useFormik({
     initialValues: {
       president: defenseDetail?.president?.toString() || "",
-      firstJuror: defenseDetail?.first_juror?.toString() || "",
-      secondJuror: defenseDetail?.second_juror?.toString() || "",
+      firstJuror: defenseDetail?.first_juror?.toString() || process?.tutor_id?.toString() || "",
+      secondJuror:
+        defenseDetail?.second_juror?.toString() || process?.reviewer_id?.toString() || "",
       date: defenseDetail?.date ? dayjs(defenseDetail.date) : dayjs(),
     },
     validationSchema: Yup.object({
@@ -69,8 +70,9 @@ export const InternalDefenseStage: FC<InternalDefenseStageProps> = ({ onPrevious
     if (defenseDetail) {
       formik.setValues({
         president: defenseDetail.president?.toString() || "",
-        firstJuror: defenseDetail.first_juror?.toString() || "",
-        secondJuror: defenseDetail.second_juror?.toString() || "",
+        firstJuror: defenseDetail.first_juror?.toString() || process?.tutor_id?.toString() || "",
+        secondJuror:
+          defenseDetail.second_juror?.toString() || process?.reviewer_id?.toString() || "",
         date: defenseDetail.date ? dayjs(defenseDetail.date) : dayjs(),
       });
     }
@@ -104,14 +106,14 @@ export const InternalDefenseStage: FC<InternalDefenseStageProps> = ({ onPrevious
 
   const saveStage = async (values: InternalValues) => {
     if (process) {
-      const defenseDetail = {
+      const defensePayload = {
         graduation_process_id: process.id,
         president: Number(values.president),
         first_juror: Number(values.firstJuror),
         second_juror: Number(values.secondJuror),
       };
       await postDefenseDetail(process.id, {
-        ...defenseDetail,
+        ...defensePayload,
         type: DEFENSE_INTERNAL,
       });
       process.stage_id = 4;
@@ -141,14 +143,13 @@ export const InternalDefenseStage: FC<InternalDefenseStageProps> = ({ onPrevious
     formik.setFieldValue("secondJuror", value?.id || "");
   };
 
-  const canApproveStage = () => {
-    return Boolean(
+  const canApproveStage = () =>
+    Boolean(
       formik.values.president &&
         formik.values.firstJuror &&
         formik.values.secondJuror &&
         formik.values.date
     );
-  };
 
   const isApproveButton = canApproveStage();
 
@@ -161,27 +162,28 @@ export const InternalDefenseStage: FC<InternalDefenseStageProps> = ({ onPrevious
   };
 
   const prevSubStage = () => {
-    if(subStage === 0)
+    if (subStage === 0) {
       onPrevious();
-    else
+    } else {
       setSubStage(subStage - 1);
+    }
   };
 
   return (
     <>
       <Typography variant="h6" gutterBottom style={{ fontWeight: "bold" }}>
-        Etapa 4: Defensa Interna{" "}
-        {subStage == 1 && <ModeEditIcon onClick={editForm} style={{ cursor: "pointer" }} />}
+        {"Etapa 4: Defensa Interna"}{" "}
+        {subStage === 1 && <ModeEditIcon onClick={editForm} style={{ cursor: "pointer" }} />}
       </Typography>
       {subStage === 0 && (
         <>
           <EmailSender />
           <Box display="flex" justifyContent="space-between" pt={1} pb={0}>
             <Button type="button" onClick={prevSubStage} variant="contained" color="secondary">
-              Anterior
+              {"Anterior"}
             </Button>
             <Button onClick={nextSubStage} variant="contained" color="primary">
-              Siguiente
+              {"Siguiente"}
             </Button>
           </Box>
         </>
@@ -196,7 +198,7 @@ export const InternalDefenseStage: FC<InternalDefenseStageProps> = ({ onPrevious
                   value={String(formik.values.president)}
                   onChange={handlePresidentChange}
                   id="president"
-                  label={"Seleccionar Presidente"}
+                  label="Seleccionar Presidente"
                 />
                 {formik.touched.president && formik.errors.president ? (
                   <div className="text-red-1 text-xs mt-1">{String(formik.errors.president)}</div>
@@ -209,7 +211,7 @@ export const InternalDefenseStage: FC<InternalDefenseStageProps> = ({ onPrevious
                   value={String(formik.values.firstJuror)}
                   onChange={handleFirstJurorChange}
                   id="firstJuror"
-                  label={"Seleccionar Primer Jurado"}
+                  label="Seleccionar Primer Jurado"
                 />
                 {formik.touched.firstJuror && formik.errors.firstJuror ? (
                   <div className="text-red-1 text-xs mt-1">{String(formik.errors.firstJuror)}</div>
@@ -222,7 +224,7 @@ export const InternalDefenseStage: FC<InternalDefenseStageProps> = ({ onPrevious
                   value={String(formik.values.secondJuror)}
                   onChange={handleSecondJurorChange}
                   id="secondJuror"
-                  label={"Seleccionar Segundo Jurado"}
+                  label="Seleccionar Segundo Jurado"
                 />
                 {formik.touched.secondJuror && formik.errors.secondJuror ? (
                   <div className="text-red-1 text-xs mt-1">{String(formik.errors.secondJuror)}</div>
@@ -247,7 +249,7 @@ export const InternalDefenseStage: FC<InternalDefenseStageProps> = ({ onPrevious
 
           <Box display="flex" justifyContent="space-between" pt={5}>
             <Button type="button" onClick={prevSubStage} variant="contained" color="secondary">
-              Anterior
+              {"Anterior"}
             </Button>
             <Button type="submit" variant="contained" color="primary">
               {isApproveButton ? "Aprobar Etapa" : "Guardar"}
@@ -267,3 +269,5 @@ export const InternalDefenseStage: FC<InternalDefenseStageProps> = ({ onPrevious
     </>
   );
 };
+
+export default InternalDefenseStage;
