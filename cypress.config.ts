06d8d1c0f4d670e1cd296e1dc55738aebc93bc3f
Danilepez <danilepez5@gmail.com>
2025-06-09 21:54:35 -0400

    Test crear docente con cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,10 +10,14 @@ export default defineConfig({
     CAREER_MANAGER_PASSWORD: '123456',
     SUPERVISOR_EMAIL: 'supervisor@gmail.com',
     SUPERVISOR_PASSWORD: '123456',
+    HEAD_OF_DEPARTMENT_EMAIL: 'alexismarechal@upb.edu',
+    HEAD_OF_DEPARTMENT_PASSWORD: '123456'
   },
   e2e: {
-    setupNodeEvents(on, config) {
+    /*setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
+    },*/
+    baseUrl: "http://localhost:5173",
+    supportFile: "cypress/support/commands.ts",
   },
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,37 +1,29 @@
-/// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+//login asHeadOfDepartment
+Cypress.Commands.add("loginAsHeadOfDepartment", () => {
+  const email = Cypress.env("HEAD_OF_DEPARTMENT_EMAIL");
+  const password = Cypress.env("HEAD_OF_DEPARTMENT_PASSWORD");
+
+  cy.visit("/login");
+  cy.get('input[name="email"]').type(email);
+  cy.get('input[name="password"]').type(password);
+  cy.contains("button", "Login").click();
+  cy.url().should("not.include", "/login");
+});
+
+// createTeacher
+Cypress.Commands.add("createTeacher", (teacher) => {
+  cy.get('[data-testid="SupervisorAccountIcon"]').should('be.visible').click();
+
+  cy.contains('Agregar docente').should('be.visible').click();
+
+  cy.get('input[name="name"]').type(teacher.name);
+  cy.get('input[name="lastname"]').type(teacher.lastname);
+  cy.get('input[name="mothername"]').type(teacher.mothername);
+  cy.get('input[name="code"]').type(teacher.code);
+  cy.get('#degree').click();
+  cy.get('ul[role="listbox"]').contains(`${teacher.degree}`).click();
+  cy.get('input[name="email"]').type(teacher.email);
+  cy.get('input[name="phone"]').type(teacher.phone);
+
+  cy.contains("button", "GUARDAR").click();
+});
